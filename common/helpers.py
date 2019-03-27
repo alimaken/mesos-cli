a76f53fafa5adbd6ef9f6914ca7ccf4388db9333
AliMaken <ali_maken@epam.com>
2019-03-27 10:23:59 +0100

    Added percentage representation to CPU and MEM usage.

---
+++ common/helpers.py
@@ -0,0 +1,17 @@
+
+class Helpers(object):
+
+    @staticmethod
+    def get_percent(value: str, precision: 2) -> float:
+        """
+        Converts a string representation of percentage of `0.00 - 1.00` to `0.00 - 100.00`
+        :param precision: Precision to be used for return float value
+        :param value: String percentage between `0.0` and `1.0`
+        :return: Float percentage between `0` and `100`
+        :rtype: float
+        """
+        if value:
+            f = float(value)
+            return round((f * 100) if (f < 1.0) else f, precision)
+        else:
+            return 0.0
